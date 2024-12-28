2f67d49364a041c35e9c92556f789a713b7ccdfd
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2024-12-28 16:00:23 +0530

    [Solve]:question base on HOF

--- revision.js
+++ revision.js
@@ -14,14 +14,14 @@ console.log( resultArry )
 // calculte total age & output should be 121
 const userDetails = [
     {
-        name: "Jay",
+        name: "sam",
         age: 60
     },
     {
-        name: "Gloria",
+        name: "karan",
         age: 36
     }, {
-        name: "Manny",
+        name: "bull",
         age: 16
     }, {
         name: "Joe",
@@ -38,4 +38,58 @@ for(let i=0; i<userDetails.length; i++){
     totalAge += userDetails[i].age;
 }
 console.log("Total age:",totalAge)
-// 35:00
+
+const family = [
+    {
+        name:"Ramesh",
+        haveCycle: true
+    },
+    {
+        name:"surbhi",
+        haveCycle: false
+    },
+    {
+        name:"Anjali",
+        haveCycle: true
+    },
+    {
+        name:"makhana",
+        haveCycle: false
+    }
+]
+
+// ensuring immutability 
+const getArrayIfCycle = (acc,member)=>member.haveCycle?[...acc,member.name]:[...acc]
+const resultList = family.reduce(getArrayIfCycle,[])
+console.log(resultList)
+// whoHasCycle
+
+// NOTE: .push change original one arry
+// OR
+// const container = family.reduce((acc,member)=>{
+//     if(member.haveCycle){
+//         acc.push(member.name)
+//     }
+//     return acc
+// },[])
+// console.log(container)
+
+// const result = family.filter((member)=>member.haveCycle).map((exe)=>exe.name)
+// console.log(result)
+
+// OR
+// const container = family.filter((member)=>member.haveCycle).reduce((acc,member)=>{
+// acc.push(member.name)
+// return acc;
+// },[])
+// console.log(container)
+
+// OR
+// let container = [];
+// for(let i=0; i<family.length; i++){
+//     if(family[i].haveCycle === true){
+//         container.push(family[i].name)
+//     }
+// }
+// console.log(container)
+// 57
